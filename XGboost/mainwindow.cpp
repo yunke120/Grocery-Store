b9c278ba96a27835ee345f9d7adfefa73b6f7212
yunke120 <fj_18860352816@163.com>
2023-02-21 09:50:00 +0800

    complete

--- XGboost/mainwindow.cpp
+++ XGboost/mainwindow.cpp
@@ -18,6 +18,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
 MainWindow::~MainWindow()
 {
+    _Py_XDECREF(pObject);
     Py_Finalize();
     delete ui;
 }
@@ -99,9 +100,8 @@ void MainWindow::on_btnPredict_clicked()
     int param1 = ui->lEParam1->text().toInt();
     int param2 = ui->lEParam2->text().toInt();
     int param3 = ui->lEParam3->text().toInt();
-    QString invest_time = ui->lEInvestTime->text();
+    QString invest_time = ui->cBInvestTime->currentText();
     QString param4 = ui->cBParam4->currentText();
-    double stock = ui->lEStock->text().toDouble();
     int param5 = ui->lEParam5->text().toInt();
     int param6 = ui->lEParam6->text().toInt();
 
@@ -113,7 +113,6 @@ void MainWindow::on_btnPredict_clicked()
     PyObject * _param3 = PyTuple_New(1); /* 创建1个元素的元组 */
     PyObject * _invest_time = PyTuple_New(1); /* 创建1个元素的元组 */
     PyObject * _param4 = PyTuple_New(1); /* 创建1个元素的元组 */
-    PyObject * _stock = PyTuple_New(1); /* 创建1个元素的元组 */
     PyObject * _param5 = PyTuple_New(1); /* 创建1个元素的元组 */
     PyObject * _param6 = PyTuple_New(1); /* 创建1个元素的元组 */
 
@@ -125,7 +124,6 @@ void MainWindow::on_btnPredict_clicked()
     PyTuple_SetItem(_param3, 0, Py_BuildValue("i", param3));
     PyTuple_SetItem(_invest_time, 0, Py_BuildValue("s", invest_time.toLatin1().data()));
     PyTuple_SetItem(_param4, 0, Py_BuildValue("s", param4.toLatin1().data()));
-    PyTuple_SetItem(_stock, 0, Py_BuildValue("f", stock));
     PyTuple_SetItem(_param5, 0, Py_BuildValue("i", param5));
     PyTuple_SetItem(_param6, 0, Py_BuildValue("i", param6));
 
@@ -133,183 +131,179 @@ void MainWindow::on_btnPredict_clicked()
     PyObject* pFun1 = PyObject_GetAttrString(pObject,"get_number"); // 获取 xg模块中的 get_number函数
     if(!pFun1){
         PyErr_Print();
-        qDebug() <<"Get function get_number failed";
+        return;
     }
     PyObject *pRet1 = PyObject_CallObject(pFun1, _number); // 调用 xg模块中的 get_number函数，传递 编号 值
-
+    _Py_XDECREF(_number);
+    _Py_XDECREF(pFun1);
     if(pRet1 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet1);
+
+
 
     // sex
     PyObject* pFun2 = PyObject_GetAttrString(pObject,"get_sex");
     if(!pFun2){
         PyErr_Print();
-        qDebug() <<"Get function get_sex failed";
+        return;
     }
     PyObject *pRet2 = PyObject_CallObject(pFun2, _sex);
-
+    _Py_XDECREF(_sex);
+    _Py_XDECREF(pFun2);
     if(pRet2 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet2);
 
     // age
     PyObject* pFun3 = PyObject_GetAttrString(pObject,"get_age");
     if(!pFun3){
         PyErr_Print();
-        qDebug() <<"Get function get_age failed";
+        return;
     }
     PyObject *pRet3 = PyObject_CallObject(pFun3, _age);
-
+    _Py_XDECREF(_age);
+    _Py_XDECREF(pFun3);
     if(pRet3 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet3);
 
     // param1
     PyObject* pFun4 = PyObject_GetAttrString(pObject,"get_param1");
     if(!pFun4){
         PyErr_Print();
-        qDebug() <<"Get function get_param1 failed";
+        return;
     }
     PyObject *pRet4 = PyObject_CallObject(pFun4, _param1);
-
+    _Py_XDECREF(_param1);
+    _Py_XDECREF(pFun4);
     if(pRet4 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet4);
 
     // param2
     PyObject* pFun5 = PyObject_GetAttrString(pObject,"get_param2");
     if(!pFun5){
         PyErr_Print();
-        qDebug() <<"Get function get_param2 failed";
+        return;
     }
     PyObject *pRet5 = PyObject_CallObject(pFun5, _param2);
-
+    _Py_XDECREF(_param2);
+    _Py_XDECREF(pFun5);
     if(pRet5 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet5);
 
     // param3
     PyObject* pFun6 = PyObject_GetAttrString(pObject,"get_param3");
     if(!pFun6){
         PyErr_Print();
-        qDebug() <<"Get function get_param3 failed";
+        return;
     }
     PyObject *pRet6 = PyObject_CallObject(pFun6, _param3);
-
+    _Py_XDECREF(_param3);
+    _Py_XDECREF(pFun6);
     if(pRet6 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet6);
 
     // investtime
     PyObject* pFun7 = PyObject_GetAttrString(pObject,"get_investtime");
     if(!pFun7){
         PyErr_Print();
-        qDebug() <<"Get function get_investtime failed";
+        return;
     }
     PyObject *pRet7 = PyObject_CallObject(pFun7, _invest_time);
-
+    _Py_XDECREF(_invest_time);
+    _Py_XDECREF(pFun7);
     if(pRet7 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet7);
 
 
     // param4
     PyObject* pFun8 = PyObject_GetAttrString(pObject,"get_param4");
     if(!pFun8){
         PyErr_Print();
-        qDebug() <<"Get function get_param4 failed";
+        return;
     }
     PyObject *pRet8 = PyObject_CallObject(pFun8, _param4);
-
+    _Py_XDECREF(_param4);
+    _Py_XDECREF(pFun8);
     if(pRet8 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
-
-
-    // stock
-    PyObject* pFun9 = PyObject_GetAttrString(pObject,"get_stock");
-    if(!pFun9){
-        PyErr_Print();
-        qDebug() <<"Get function get_stock failed";
-    }
-    PyObject *pRet9 = PyObject_CallObject(pFun9, _stock);
-
-    if(pRet9 == NULL)
-    {
-        qDebug() << __LINE__;
-        PyErr_Print();
-        return;
-    }
+    _Py_XDECREF(pRet8);
 
     // param5
     PyObject* pFun10 = PyObject_GetAttrString(pObject,"get_param5");
     if(!pFun10){
         PyErr_Print();
-        qDebug() <<"Get function get_param5 failed";
+        return;
     }
     PyObject *pRet10 = PyObject_CallObject(pFun10, _param5);
-
+    _Py_XDECREF(_param5);
+    _Py_XDECREF(pFun10);
     if(pRet10 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet10);
 
     // param6
     PyObject* pFun11 = PyObject_GetAttrString(pObject,"get_param6");
     if(!pFun11){
         PyErr_Print();
-        qDebug() <<"Get function get_param6 failed";
+        return;
     }
     PyObject *pRet11 = PyObject_CallObject(pFun11, _param6);
-
+    _Py_XDECREF(_param6);
+    _Py_XDECREF(pFun11);
     if(pRet11 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet11);
 
 
     PyObject* pFun12 = PyObject_GetAttrString(pObject,"predict1"); // 调用预测函数
     if(!pFun12){
         PyErr_Print();
-        qDebug() <<"Get function predict1 failed";
         return;
     }
     PyObject *pRet12 = PyObject_CallObject(pFun12, NULL);
+    _Py_XDECREF(pFun12);
     if(pRet12 == NULL)
     {
-        qDebug() << __LINE__;
         PyErr_Print();
         return;
     }
+    _Py_XDECREF(pRet12);
     double result = PyFloat_AsDouble(pRet12); // 解析返回值
     
     ui->lEResult->setText(QString::number(result)); // 将预测结果显示在界面上
@@ -327,20 +321,20 @@ void MainWindow::on_btnTrain_clicked()
 
     PyObject * csv_file = PyTuple_New(1); /* 创建1个元素的元组 */
     PyTuple_SetItem(csv_file, 0, Py_BuildValue("s", m_TrainFile.toLatin1().data()));
-
     PyObject* pFun = PyObject_GetAttrString(pObject,"train_model");
     if(!pFun){
         PyErr_Print();
         qDebug() <<"Get function train_model failed";
         return;
     }
-
     ui->labelStatus->setText(QStringLiteral("训练中，请勿操作..."));
     QApplication::processEvents(); // 先处理还没有处理的事件，主要是为了显示上一句代码
     PyObject *pRet = PyObject_CallObject(pFun, csv_file);
+    _Py_XDECREF(csv_file);
+    _Py_XDECREF(pFun);
     if(pRet == NULL)
     {
-
+        _Py_XDECREF(pFun);
         qDebug() << __LINE__;
         PyErr_Print();
         return;
@@ -351,6 +345,7 @@ void MainWindow::on_btnTrain_clicked()
         m_Flag = true;
         ui->labelStatus->setText(QStringLiteral("训练完成"));
     }
+    _Py_XDECREF(pRet);
 }
 
 // 选择文件槽函数
